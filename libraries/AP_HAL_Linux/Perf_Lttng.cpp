f12135dbf979fe6e2c8949ba74343465a1a5f1f5
mirkix <mirkix@gmail.com>
2017-02-17 21:10:20 +0100

    lttng: enable lttng

--- libraries/AP_HAL_Linux/Perf_Lttng.cpp
+++ libraries/AP_HAL_Linux/Perf_Lttng.cpp
@@ -12,6 +12,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#define HAVE_LTTNG_UST
 #ifdef HAVE_LTTNG_UST
 
 #define TRACEPOINT_CREATE_PROBES
