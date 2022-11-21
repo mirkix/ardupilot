65743ad264d16b5d49bc39138b5ba84b54a56d04
Mirko Denecke <mirkix@gmail.com>
2022-11-21 09:46:56 +0100

    AP_HAL_ChibiOS: mixer 19.11.2022 update

--- libraries/AP_HAL_ChibiOS/hwdef/CarryAir/scripts/mixer.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/CarryAir/scripts/mixer.lua
@@ -8,8 +8,8 @@ local AP_MOTORS_MOT_4 = 3
 local AP_MOTORS_MOT_5 = 4
 local AP_MOTORS_MOT_6 = 5
 
-MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_1, -1.0, -0.073, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 1);
-MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_2, 1.0, -0.073, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
+MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_1, -1.0, 0.0, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 1);
+MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_2, 1.0, 0.0, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
 MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_3, 0.424, 0.533, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 3);
 MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_4, -0.424, -0.667, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
 MotorsMatrix:add_motor_raw(AP_MOTORS_MOT_5, -0.424, 0.533, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
