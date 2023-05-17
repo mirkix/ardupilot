1425d19595cef91c4136f858029e2adc93630f7c
Mirko Denecke <mirkix@gmail.com>
2023-05-17 14:26:54 +0200

    AP_HAL_ChibiOS: add CarrayAirPlus

---
+++ libraries/AP_HAL_ChibiOS/hwdef/CarryAirPlus/scripts/carry.lua
@@ -0,0 +1,83 @@
+local THR_SERVO_1 = 6 -- SERVO7
+local THR_SERVO_2 = 7 -- SERVO8
+local THR_SERVO = 70
+
+local PARAM_TABLE_KEY = 25
+assert(param:add_table(PARAM_TABLE_KEY, "CA_", 30), 'could not add param table')
+
+assert(param:add_param(PARAM_TABLE_KEY, 1,  'VFWD_UP', 0.15), 'could not add param1')
+assert(param:add_param(PARAM_TABLE_KEY, 2,  'VFWD_DOWN', 0.05), 'could not add param2')
+assert(param:add_param(PARAM_TABLE_KEY, 3,  'VFWD_DEFAULT', 0.1), 'could not add param3')
+assert(param:add_param(PARAM_TABLE_KEY, 4,  'DCRT_UP', 150.0), 'could not add param4')
+assert(param:add_param(PARAM_TABLE_KEY, 5,  'DCRT_DOWN', -100.0), 'could not add param5')
+assert(param:add_param(PARAM_TABLE_KEY, 6,  'DCRT_AUTO', 0.0), 'could not add param6')
+assert(param:add_param(PARAM_TABLE_KEY, 7,  'BOOST_PCT', 0.0), 'could not add param7')
+assert(param:add_param(PARAM_TABLE_KEY, 8,  'TRA_THR_PCT', 0.0), 'could not add param8')
+
+local CA_VFWD_UP = Parameter()
+CA_VFWD_UP:init('CA_VFWD_UP')
+
+local CA_VFWD_DOWN = Parameter()
+CA_VFWD_DOWN:init('CA_VFWD_DOWN')
+
+local CA_VFWD_DEFAULT = Parameter()
+CA_VFWD_DEFAULT:init('CA_VFWD_DEFAULT')
+
+local CA_DCRT_UP = Parameter()
+CA_DCRT_UP:init('CA_DCRT_UP')
+
+local CA_DCRT_DOWN = Parameter()
+CA_DCRT_DOWN:init('CA_DCRT_DOWN')
+
+local CA_DCRT_AUTO = Parameter()
+CA_DCRT_AUTO:init('CA_DCRT_AUTO')
+
+local CA_BOOST_PCT = Parameter()
+CA_BOOST_PCT:init('CA_BOOST_PCT')
+
+local CA_TRA_THR_PCT = Parameter()
+CA_TRA_THR_PCT:init('CA_TRA_THR_PCT')
+
+local Q_VFWD_GAIN = Parameter()
+Q_VFWD_GAIN:init('Q_VFWD_GAIN')
+
+if((SRV_Channels:channel_function(THR_SERVO_1) == THR_SERVO) and (SRV_Channels:channel_function(THR_SERVO_2) == THR_SERVO)) then
+    gcs:send_text(6, "THR_SERVO check ok")
+else
+    gcs:send_text(6, "THR_SERVO check NOT ok")
+end
+
+function transition_throttle()
+    if vehicle:get_likely_flying() and quadplane:in_transition() and CA_TRA_THR_PCT:get() > 0.1 then
+        local pwm_value = math.floor(1000 + (CA_TRA_THR_PCT:get() * 10.0))
+        if (SRV_Channels:channel_function(THR_SERVO_1) == THR_SERVO) and (SRV_Channels:channel_function(THR_SERVO_2) == THR_SERVO) then
+            SRV_Channels:set_output_pwm_chan_timeout(THR_SERVO_1, pwm_value, 600)
+            SRV_Channels:set_output_pwm_chan_timeout(THR_SERVO_2, pwm_value, 600)
+        end
+    end
+    return dcrt_adjust, 500
+end
+
+function dcrt_adjust()
+    if quadplane:in_vtol_mode() and vehicle:get_likely_flying() and CA_DCRT_AUTO:get() > 0.1 then
+        local dcrt = quadplane:get_vel_target_z_cms()
+--        gcs:send_text(0, "dCRT: " .. dcrt)
+--        gcs:send_text(0, "Q_VFWD_GAIN: " .. Q_VFWD_GAIN:get())
+        if dcrt > CA_DCRT_UP:get() then
+            Q_VFWD_GAIN:set(CA_VFWD_UP:get())
+            quadplane:set_boost_pct(CA_BOOST_PCT:get())
+        elseif dcrt <  CA_DCRT_DOWN:get() then
+            Q_VFWD_GAIN:set(CA_VFWD_DOWN:get())
+            quadplane:set_boost_pct(0.0)
+
+        else
+            Q_VFWD_GAIN:set(CA_VFWD_DEFAULT:get())
+            quadplane:set_boost_pct(0.0)
+        end
+    end
+    return transition_throttle, 1
+end
+
+gcs:send_text(6, "Start CarryAir LUA Script")
+
+return dcrt_adjust, 1000
---
+++ libraries/AP_HAL_ChibiOS/hwdef/CarryAirPlus/scripts/gt33reu.lua
@@ -0,0 +1,70 @@
+local scr_user1_param = Parameter()
+assert(scr_user1_param:init('SCR_USER1'), 'could not find SCR_USER1 parameter')
+
+if scr_user1_param:get() == 0 then
+    gcs:send_text(6,"GT33REU LUA script disabled")
+    return
+end
+
+gcs:send_text(6,"Start GT33REU LUA script")
+
+local driver = CAN.get_device(20)
+
+local div = 1
+local fuel = 0
+local temp1 = 0
+local power = 0
+local current2 = 0
+local rpm = 0
+
+--logger.write('GT33','temp(deg),current2(a),power(w)','iii',temp1,current2,power)
+
+function read_i16(b1, b2)
+    assert (0 <= b1 and b1 <= 0xff)
+    assert (0 <= b2 and b2 <= 0xff)
+    local mask = (1 << 15)
+    local res  = (b1 << 8) | (b2 << 0)
+    return (res ~ mask) - mask
+end
+
+if not driver then
+    gcs:send_text(0,"No GT33REU CAN interface found")
+    return
+end
+
+function show_frame(dnum, frame)
+    if frame:id() == uint32_t(0x80) then
+        temp1 = read_i16(frame:data(4), frame:data(5))
+        rpm = (frame:data(0) << 8) | frame:data(1)
+    elseif frame:id() == uint32_t(0x81) then
+        current2 = read_i16(frame:data(6), frame:data(7))
+    elseif frame:id() == uint32_t(0x82) then
+        fuel = (frame:data(0) << 8) | frame:data(1)
+    elseif frame:id() == uint32_t(0x83) then
+        power = (frame:data(4) << 8) | frame:data(5)
+    end
+
+    if div < 2 then
+        gcs:send_text(6,string.format("T1: %d C°,C2: %d A,Pwr: %d W,RPM: %d, Fuel: %d", temp1, current2, power, rpm, fuel))
+        div = 1000
+    else
+        div = div - 1
+    end
+--    logger.write('GT33','temp(deg),current2(a),power(w)','iii',temp1,current2,power)
+end
+
+function update()
+    if driver then
+        frame = driver:read_frame()
+        if frame then
+            show_frame(1, frame)
+        end
+        return update, 10
+    end
+    gcs:send_text(0,"Stop GT33REU LUA script")
+    return
+end
+
+gcs:send_text(6, "GT33REU driver loaded")
+
+return update()
